16475a1f37835ecd51b90efeb82427e65983e35b
Rafael Zanetti <rfzanetti@gmail.com>
2015-05-31 18:12:27 -0300

    Adicionado SubdominioBean
    o primeiro de vários

---
+++ model/SubdominioBean.java
@@ -0,0 +1,22 @@
+package model;
+
+public class SubdominioBean {
+  private String nomeSubdominio;
+  private Float gastoTotal;   
+  
+ /* public SubdominioBean(){
+    type = "regular";
+  }   */
+  public String getName() {
+    return this.nomeSubdominio;
+  }
+  public void setName(String name) {
+    this.nomeSubdominio = name;
+  }
+  public Float getGastoTotal() {
+    return this.gastoTotal;
+  }
+  public void setType(Float valor) {
+    this.gastoTotal = valor;
+  }
+}
