eccbe99b3657e4bd1016eabea0a071dd6b33405d
angelarf <angela.rod.fe@gmail.com>
2015-06-02 00:57:08 -0300

    Beans adicionados

---
+++ src/java/model/DominioBean.java
@@ -0,0 +1,26 @@
+package model;
+
+public class DominioBean {
+    
+  private String nomePrograma;
+  private Float gasto;   
+
+    public String getNomePrograma() {
+        return nomePrograma;
+    }
+
+    public void setNomePrograma(String nomePrograma) {
+        this.nomePrograma = nomePrograma;
+    }
+
+    public Float getGasto() {
+        return gasto;
+    }
+
+
+    public void setGasto(Float gasto) {
+        this.gasto = gasto;
+    }
+  
+ 
+}
---
+++ src/java/model/ProgramaBean.java
@@ -0,0 +1,24 @@
+
+public class ProgramaBean {
+  private String nomePrograma;
+  private Float gastoTotal;   
+
+    public String getNomePrograma() {
+        return nomePrograma;
+    }
+
+    public void setNomePrograma(String nomePrograma) {
+        this.nomePrograma = nomePrograma;
+    }
+
+   
+    public Float getGastoTotal() {
+        return gastoTotal;
+    }
+
+   
+    public void setGastoTotal(Float gastoTotal) {
+        this.gastoTotal = gastoTotal;
+    }
+ 
+}
